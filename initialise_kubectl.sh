5ffa43ea38def12e947549065e339920556fe346
Mick Frost <michael.frost@avaloq.com>
2018-11-12 16:14:28 +0000

    strengthened error reporting in script

--- initialise_kubectl.sh
+++ initialise_kubectl.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env sh
 
+usage() {
+  echo "$0 <project> <cluster> <zone>"
+  exit 1
+}
+
 project="$1"
 cluster="$2"
 zone="$3"
+if [ -z "$project" ]; then echo "project is unset" && usage; fi
+if [ -z "$cluster" ]; then echo "cluster is unset" && usage; fi
+if [ -z "$zone" ]; then echo "zone is unset" && usage; fi
 
 gcloud components install --quiet kubectl
 gcloud container clusters get-credentials "$cluster" --zone "$zone"
