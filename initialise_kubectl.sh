4ed92820ad4fe0222b6987bb38df5f3f87d07273
Mick Frost <michael.frost@avaloq.com>
2018-11-13 12:46:43 +0000

    Logged in as sys admin

--- initialise_kubectl.sh
+++ initialise_kubectl.sh
@@ -1,17 +1,22 @@
 #!/usr/bin/env sh
 
 usage() {
-  echo "$0 <project> <cluster> <zone>"
+  echo "$0 <project> <cluster> <zone> <admin_password>"
   exit 1
 }
 
 project="$1"
 cluster="$2"
 zone="$3"
+password="$4"
 if [ -z "$project" ]; then echo "project is unset" && usage; fi
 if [ -z "$cluster" ]; then echo "cluster is unset" && usage; fi
 if [ -z "$zone" ]; then echo "zone is unset" && usage; fi
+if [ -z "$password" ]; then echo "Admin password is unset" && usage; fi
 
 gcloud components install --quiet kubectl
 gcloud container clusters get-credentials "$cluster" --zone "$zone"
+kubectl config set-credentials "admin/$cluster" --username=admin --password="$password"
+kubectl config set-context sysadmin --user="admin/$cluster" --cluster="gke_$project_$zone_$cluster"
+kubectl config use-context sysadmin
 kubectl cluster-info
