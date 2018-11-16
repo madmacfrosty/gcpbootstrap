e492936622af6446294e18a4d49008fc19457a41
Mick Frost <michael.frost@avaloq.com>
2018-11-16 10:12:35 +0000

    Fixing cleanup

--- scripts/clean_up.sh
+++ scripts/clean_up.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env sh
 
 kubectl delete namespace platform
-kubectl delete clusterrole my-release-etcd-operator-etcd-operator
+kubectl delete clusterrole etcd-release-etcd-operator-etcd-operator
 kubectl delete clusterrolebinding tiller-cluster-admin-binding
-kubectl delete clusterrolebinding my-release-etcd-operator-etcd-backup-operator
-kubectl delete clusterrolebinding my-release-etcd-operator-etcd-operator
-kubectl delete clusterrolebinding my-release-etcd-operator-etcd-restore-operator
+kubectl delete clusterrolebinding etcd-release-etcd-operator-etcd-backup-operator
+kubectl delete clusterrolebinding etcd-release-etcd-operator-etcd-operator
+kubectl delete clusterrolebinding etcd-release-etcd-operator-etcd-restore-operator
 kubectl delete crd etcdbackups.etcd.database.coreos.com
 kubectl delete crd etcdclusters.etcd.database.coreos.com
 kubectl delete crd etcdrestores.etcd.database.coreos.com
+kubectl delete crd vaultservices.vault.security.coreos.com
